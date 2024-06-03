ac0fdabf2db277eaa6ba4dd6551c4c85dff00c9f
natesanow <152323081+natesanow@users.noreply.github.com>
2024-06-03 09:35:03 -0700

    edited code in search books page

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -23,6 +23,8 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
+
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
@@ -38,8 +40,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
-
+      const response = await fetch (
+        `https://www.googleapis.com/books/v1/volumes?q=${searchInput}`
+      );
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
